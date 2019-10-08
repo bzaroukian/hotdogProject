bbe3bbfcf828c342e2a582315436c6b63cf1cf7a
bzaroukian <bzucchini@gmail.com>
2019-10-07 21:35:34 -0400

    inital commit

---
+++ Hotdog_Stand/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace Hotdog_Stand
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.sideButton = new System.Windows.Forms.Button();
+            this.bbqChipsCheckBox2 = new System.Windows.Forms.CheckBox();
+            this.planChipsCheckBox = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.hotdoButton = new System.Windows.Forms.Button();
+            this.mustardCheckBox = new System.Windows.Forms.CheckBox();
+            this.ketchupCheckBox = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.locationTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.orderListBox = new System.Windows.Forms.ListBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.drinkButton = new System.Windows.Forms.Button();
+            this.waterCheckBox = new System.Windows.Forms.CheckBox();
+            this.spriteCheckBox1 = new System.Windows.Forms.CheckBox();
+            this.regularCheckBox = new System.Windows.Forms.CheckBox();
+            this.dietPopCheckBox = new System.Windows.Forms.CheckBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.checkoutButton = new System.Windows.Forms.Button();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.sideButton);
+            this.groupBox3.Controls.Add(this.bbqChipsCheckBox2);
+            this.groupBox3.Controls.Add(this.planChipsCheckBox);
+            this.groupBox3.Location = new System.Drawing.Point(513, 152);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 100);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Sides";
+            // 
+            // sideButton
+            // 
+            this.sideButton.Location = new System.Drawing.Point(7, 67);
+            this.sideButton.Name = "sideButton";
+            this.sideButton.Size = new System.Drawing.Size(86, 24);
+            this.sideButton.TabIndex = 2;
+            this.sideButton.Text = "Add Sides";
+            this.sideButton.UseVisualStyleBackColor = true;
+            this.sideButton.Click += new System.EventHandler(this.SideButton_Click);
+            // 
+            // bbqChipsCheckBox2
+            // 
+            this.bbqChipsCheckBox2.AutoSize = true;
+            this.bbqChipsCheckBox2.Location = new System.Drawing.Point(7, 44);
+            this.bbqChipsCheckBox2.Name = "bbqChipsCheckBox2";
+            this.bbqChipsCheckBox2.Size = new System.Drawing.Size(77, 17);
+            this.bbqChipsCheckBox2.TabIndex = 1;
+            this.bbqChipsCheckBox2.Text = "BBQ Chips";
+            this.bbqChipsCheckBox2.UseVisualStyleBackColor = true;
+            // 
+            // planChipsCheckBox
+            // 
+            this.planChipsCheckBox.AutoSize = true;
+            this.planChipsCheckBox.Location = new System.Drawing.Point(7, 20);
+            this.planChipsCheckBox.Name = "planChipsCheckBox";
+            this.planChipsCheckBox.Size = new System.Drawing.Size(86, 17);
+            this.planChipsCheckBox.TabIndex = 0;
+            this.planChipsCheckBox.Text = "Potato Chips";
+            this.planChipsCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.hotdoButton);
+            this.groupBox2.Controls.Add(this.mustardCheckBox);
+            this.groupBox2.Controls.Add(this.ketchupCheckBox);
+            this.groupBox2.Location = new System.Drawing.Point(513, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 109);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Hotdog";
+            // 
+            // hotdoButton
+            // 
+            this.hotdoButton.Location = new System.Drawing.Point(7, 68);
+            this.hotdoButton.Name = "hotdoButton";
+            this.hotdoButton.Size = new System.Drawing.Size(89, 23);
+            this.hotdoButton.TabIndex = 2;
+            this.hotdoButton.Text = "Add Hotdog";
+            this.hotdoButton.UseVisualStyleBackColor = true;
+            this.hotdoButton.Click += new System.EventHandler(this.HotdoButton_Click);
+            // 
+            // mustardCheckBox
+            // 
+            this.mustardCheckBox.AutoSize = true;
+            this.mustardCheckBox.Location = new System.Drawing.Point(7, 44);
+            this.mustardCheckBox.Name = "mustardCheckBox";
+            this.mustardCheckBox.Size = new System.Drawing.Size(89, 17);
+            this.mustardCheckBox.TabIndex = 1;
+            this.mustardCheckBox.Text = "With Mustard";
+            this.mustardCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // ketchupCheckBox
+            // 
+            this.ketchupCheckBox.AutoSize = true;
+            this.ketchupCheckBox.Location = new System.Drawing.Point(7, 20);
+            this.ketchupCheckBox.Name = "ketchupCheckBox";
+            this.ketchupCheckBox.Size = new System.Drawing.Size(91, 17);
+            this.ketchupCheckBox.TabIndex = 0;
+            this.ketchupCheckBox.Text = "With Ketchup";
+            this.ketchupCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.locationTextBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.totalLabel);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.orderListBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(443, 310);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Order Info";
+            // 
+            // locationTextBox
+            // 
+            this.locationTextBox.Location = new System.Drawing.Point(244, 274);
+            this.locationTextBox.Name = "locationTextBox";
+            this.locationTextBox.Size = new System.Drawing.Size(100, 20);
+            this.locationTextBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(186, 274);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Location:";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.totalLabel.Location = new System.Drawing.Point(43, 273);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(100, 24);
+            this.totalLabel.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 274);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Total:";
+            // 
+            // orderListBox
+            // 
+            this.orderListBox.FormattingEnabled = true;
+            this.orderListBox.Location = new System.Drawing.Point(7, 20);
+            this.orderListBox.Name = "orderListBox";
+            this.orderListBox.Size = new System.Drawing.Size(415, 199);
+            this.orderListBox.TabIndex = 0;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.drinkButton);
+            this.groupBox4.Controls.Add(this.waterCheckBox);
+            this.groupBox4.Controls.Add(this.spriteCheckBox1);
+            this.groupBox4.Controls.Add(this.regularCheckBox);
+            this.groupBox4.Controls.Add(this.dietPopCheckBox);
+            this.groupBox4.Location = new System.Drawing.Point(513, 280);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(200, 100);
+            this.groupBox4.TabIndex = 6;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Drinks";
+            // 
+            // drinkButton
+            // 
+            this.drinkButton.Location = new System.Drawing.Point(7, 67);
+            this.drinkButton.Name = "drinkButton";
+            this.drinkButton.Size = new System.Drawing.Size(86, 24);
+            this.drinkButton.TabIndex = 4;
+            this.drinkButton.Text = "Add Drinks";
+            this.drinkButton.UseVisualStyleBackColor = true;
+            this.drinkButton.Click += new System.EventHandler(this.DrinkButton_Click);
+            // 
+            // waterCheckBox
+            // 
+            this.waterCheckBox.AutoSize = true;
+            this.waterCheckBox.Location = new System.Drawing.Point(77, 44);
+            this.waterCheckBox.Name = "waterCheckBox";
+            this.waterCheckBox.Size = new System.Drawing.Size(55, 17);
+            this.waterCheckBox.TabIndex = 3;
+            this.waterCheckBox.Text = "Water";
+            this.waterCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // spriteCheckBox1
+            // 
+            this.spriteCheckBox1.AutoSize = true;
+            this.spriteCheckBox1.Location = new System.Drawing.Point(78, 19);
+            this.spriteCheckBox1.Name = "spriteCheckBox1";
+            this.spriteCheckBox1.Size = new System.Drawing.Size(53, 17);
+            this.spriteCheckBox1.TabIndex = 2;
+            this.spriteCheckBox1.Text = "Sprite";
+            this.spriteCheckBox1.UseVisualStyleBackColor = true;
+            // 
+            // regularCheckBox
+            // 
+            this.regularCheckBox.AutoSize = true;
+            this.regularCheckBox.Location = new System.Drawing.Point(7, 44);
+            this.regularCheckBox.Name = "regularCheckBox";
+            this.regularCheckBox.Size = new System.Drawing.Size(63, 17);
+            this.regularCheckBox.TabIndex = 1;
+            this.regularCheckBox.Text = "Regular";
+            this.regularCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // dietPopCheckBox
+            // 
+            this.dietPopCheckBox.AutoSize = true;
+            this.dietPopCheckBox.Location = new System.Drawing.Point(7, 20);
+            this.dietPopCheckBox.Name = "dietPopCheckBox";
+            this.dietPopCheckBox.Size = new System.Drawing.Size(45, 17);
+            this.dietPopCheckBox.TabIndex = 0;
+            this.dietPopCheckBox.Text = "Diet";
+            this.dietPopCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(236, 358);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(136, 62);
+            this.clearButton.TabIndex = 8;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            // 
+            // checkoutButton
+            // 
+            this.checkoutButton.Location = new System.Drawing.Point(71, 358);
+            this.checkoutButton.Name = "checkoutButton";
+            this.checkoutButton.Size = new System.Drawing.Size(136, 62);
+            this.checkoutButton.TabIndex = 7;
+            this.checkoutButton.Text = "Checkout";
+            this.checkoutButton.UseVisualStyleBackColor = true;
+            this.checkoutButton.Click += new System.EventHandler(this.CheckoutButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.checkoutButton);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button sideButton;
+        private System.Windows.Forms.CheckBox bbqChipsCheckBox2;
+        private System.Windows.Forms.CheckBox planChipsCheckBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button hotdoButton;
+        private System.Windows.Forms.CheckBox mustardCheckBox;
+        private System.Windows.Forms.CheckBox ketchupCheckBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox locationTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox orderListBox;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button drinkButton;
+        private System.Windows.Forms.CheckBox waterCheckBox;
+        private System.Windows.Forms.CheckBox spriteCheckBox1;
+        private System.Windows.Forms.CheckBox regularCheckBox;
+        private System.Windows.Forms.CheckBox dietPopCheckBox;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button checkoutButton;
+    }
+}
+
---
+++ Hotdog_Stand/Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+
+namespace Hotdog_Stand
+{
+    public partial class Form1 : Form
+    {
+        int price = 0;
+        int itemNumber = 0;
+        string[] myArray;
+        int size; 
+        
+        public Form1()
+        {
+            InitializeComponent();
+            if (!File.Exists("HotdogOrder.txt"))
+            {
+                using (StreamWriter sw = File.CreateText("HotdogOrder.txt"))
+                {
+
+                    sw.WriteLine("Start of Order");
+
+                }
+            }
+        }
+
+        private void HotdoButton_Click(object sender, EventArgs e)
+        {
+            string hotdog = "$1 hotdog";
+            string path = @"HotdogOrder.txt";
+            int quanity = 0;
+
+
+
+            if (ketchupCheckBox.Checked == true)
+            {
+                hotdog = hotdog + " With Ketchup";
+            }
+            if (mustardCheckBox.Checked == true)
+            {
+                hotdog = hotdog + " With Mustard";
+            }
+
+            
+
+            quanity = quanity + 1;
+            price = price + 1;
+            totalLabel.Text = price.ToString();
+            ketchupCheckBox.Checked = false;
+            mustardCheckBox.Checked = false;
+
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+
+                    sw.WriteLine(hotdog);
+
+                }
+            }
+
+            if (File.Exists(path))
+            {
+                using (StreamWriter sw = new StreamWriter(path,true))
+                {
+
+                    sw.WriteLine(hotdog);
+
+                }
+            }
+
+            //myArray = new string[size];
+            //myArray[itemNumber] = hotdog;
+            //StreamReader inputFile;
+
+            //inputFile = File.OpenText("HotdogOrder.txt");
+            //string item = "";
+            //while (!inputFile.EndOfStream)
+            //{
+            //    for (int i = 0; i < myArray.Length; i++)
+            //    {
+            //        item = inputFile.ReadLine();
+            //        myArray[i]=item;
+
+
+            //    }
+            //}
+            //orderDetails.Add(inputFile.ReadLine(itemNumber));
+
+            //orderListBox.Items.Add(myArray[itemNumber]);
+            orderListBox.Items.Clear();
+            StreamReader inputFile = new StreamReader("HotdogOrder.txt");
+            string line;
+            while ((line = inputFile.ReadLine()) != null)
+            {
+                orderListBox.Items.Add(line);
+            }
+            inputFile.Close();
+
+            itemNumber++;
+            size = size + 1; 
+            
+        }
+
+        private void SideButton_Click(object sender, EventArgs e)
+        {
+            string chips = "";
+            string path = @"HotdogOrder.txt";
+            int quanity = 0;
+
+            if (planChipsCheckBox.Checked == true)
+            {
+                chips = "$1 Plain BBQ Chips";
+            }
+
+            if (bbqChipsCheckBox2.Checked == true)
+            {
+                chips = "$1 BBQ Chips";
+            }
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+
+                    sw.WriteLine(chips);
+
+                }
+            }
+
+            if (File.Exists(path))
+            {
+                using (StreamWriter sw = new StreamWriter(path,true))
+                {
+
+                    sw.WriteLine(chips);
+
+                }
+            }
+
+
+            quanity = quanity + 1;
+            planChipsCheckBox.Checked = false;
+            bbqChipsCheckBox2.Checked = false;
+            price = price + 1;
+            totalLabel.Text = price.ToString();
+
+            orderListBox.Items.Clear();
+            StreamReader inputFile = new StreamReader("HotdogOrder.txt");
+            string line;
+            while ((line = inputFile.ReadLine()) != null)
+            {
+                orderListBox.Items.Add(line);
+            }
+            inputFile.Close();
+
+            //StreamReader inputFile;
+
+            // inputFile = File.OpenText("HotdogOrder.txt");
+            //orderListBox.Items.Add(itemNumber);
+            itemNumber++;
+        }
+
+        private void DrinkButton_Click(object sender, EventArgs e)
+        {
+            string drink = "";
+            string path = @"HotdogOrder.txt";
+            int quanity = 0;
+
+            if (dietPopCheckBox.Checked == true)
+            {
+                drink = "$1 Diet";
+            }
+
+            if (regularCheckBox.Checked == true)
+            {
+                drink = "$1 Regular";
+            }
+
+            if (spriteCheckBox1.Checked == true)
+            {
+                drink = "$1 Sprite";
+            }
+
+            if (waterCheckBox.Checked == true)
+            {
+                drink = "$1 Water";
+            }
+
+            if (File.Exists(path))
+            {
+                using (StreamWriter sw = new StreamWriter(path, true))
+                {
+
+                    sw.WriteLine(drink);
+
+                }
+            }
+
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+
+                    sw.WriteLine(drink);
+
+                }
+            }
+
+            
+
+            quanity = quanity + 1;
+            dietPopCheckBox.Checked = false;
+            regularCheckBox.Checked = false;
+            spriteCheckBox1.Checked = false;
+            waterCheckBox.Checked = false;
+            price = price + 1;
+            totalLabel.Text = "$" + price.ToString();
+
+            orderListBox.Items.Clear();
+            StreamReader inputFile = new StreamReader("HotdogOrder.txt");
+            string line;
+            while ((line = inputFile.ReadLine()) != null)
+            {
+                orderListBox.Items.Add(line);
+            }
+            inputFile.Close();
+
+            //StreamReader inputFile;
+
+            //inputFile = File.OpenText("HotdogOrder.txt");
+            //orderListBox.Items.Add(itemNumber);
+            itemNumber++;
+        }
+
+        private void CheckoutButton_Click(object sender, EventArgs e)
+        {
+            string path = @"HotdogOrder.txt";
+            string newPath = @"HotdogOrder" + DateTime.Now.ToFileTime() + ".txt";
+            string location = locationTextBox.Text;
+            if (File.Exists(path))
+            {
+                using (StreamWriter sw = new  StreamWriter(path,true))
+                {
+
+                    sw.WriteLine("Please delvier this to " + location);
+                    sw.WriteLine("The order total is: " + price.ToString());
+
+                }
+            }
+
+            MessageBox.Show("The Total is $ " + price);
+            
+
+            StreamReader inputFile = new StreamReader("HotdogOrder.txt");
+            string line;
+            orderListBox.Items.Clear();
+            while ((line = inputFile.ReadLine()) !=null)
+            {
+                orderListBox.Items.Add(line);
+            }
+              
+            MessageBox.Show("Press OK when order is complete to start new transaction.");
+            inputFile.Close();
+            orderListBox.Items.Clear();
+            totalLabel.Text = "";
+            try
+            {
+               
+                System.IO.File.Move(path, newPath);
+
+            }
+            catch
+            {
+
+                GC.Collect();
+            }
+            
+
+
+
+
+        }
+    }
+}
